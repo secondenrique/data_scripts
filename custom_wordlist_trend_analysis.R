9499b73d58211fbd885a7077208b3567ead72099
secondenrique <72679186+secondenrique@users.noreply.github.com>
2021-08-23 10:21:09 -0700

    Update custom_wordlist_trend_analysis.R

--- custom_wordlist_trend_analysis.R
+++ custom_wordlist_trend_analysis.R
@@ -6,7 +6,7 @@ library(scales)
 
 # Read in data that has already been filtered through full_gloss, then format dates.
 dat <- read.csv("data.csv", stringsAsFactors = FALSE)
-dat$publishDate <- as.Date(dat$publishDate)
+dat$DATEposted <- as.Date(dat$DATEposted)
 dat <- as_tibble(dat)
 
 # Add new categories from glossary terms here
@@ -16,22 +16,22 @@ as_virulent <- c("zionazi", "shlomo")
 
 # Filter for specific rhetoric using relevant gloss terms--when adding a new target of concern per client's protected group designation, grey out unneeded lists and update as needed.
 dat1 <- dat %>%
-  select(publishDate, term) %>%
+  select(DATEposted, term) %>%
   unnest(term) %>%
   filter(term %in% antisemitic) # Replace object after %in% with list of terms needed.
   
 # Changes comment publication date to YYYY-MM format.  Adjust as needed for daily, yearly, or hourly analysis.
-dat$publishDate <- floor_date(dat$publishDate, unit="week")
-dat1$publishDate <- floor_date(dat1$publishDate, unit="week")
+dat$DATEposted <- floor_date(dat$DATEposted, unit="week")
+dat1$DATEposted <- floor_date(dat1$DATEposted, unit="week")
 
 
 
 # Frequency of each term's use in a given month.
-termfreq <- count(dat1, vars=c("publishDate", "term"))
+termfreq <- count(dat1, vars=c("DATEposted", "term"))
 
 # Frequency of total occurrances of targeted vocabulary in a given month.
-monthfreq <- count(dat$publishDate)
-monthfreq1 <- count(dat1$publishDate)
+monthfreq <- count(dat$DATEposted)
+monthfreq1 <- count(dat1$DATEposted)
 
 # Use for tracking density of comments in a given category--un-grey lines as appropriate
 dens <- monthfreq1$freq/monthfreq$freq 
@@ -60,7 +60,7 @@ ggplot(density, aes(x, dens)) +
 #geom_smooth()
 
 # Plots a frequency chart that specifies term usage.
-ggplot(termfreq, aes(publishDate, freq)) +
+ggplot(termfreq, aes(DATEposted, freq)) +
   geom_point() +
   geom_text(aes(label=term)) +
   geom_smooth()
